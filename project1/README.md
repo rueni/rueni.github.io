f9b911f41a17a6a879b0e61eadd0a610de13813e
Christopher Kim <ckim@CKim-MacBook-Pro.local>
2015-07-15 16:01:57 -0500

    css fixes

--- project1/README.md
+++ project1/README.md
@@ -1,2 +1,23 @@
-# jQueryBoilerplate
-A simple jQuery Boilerplate for students to use!
+# Professional Website
+
+The first project of WDI is to build a professional website.  I chose to build a website for a local cycling team and the steps taken to reach the end product are outlined below.
+
+1. **Purpose:**<br>
+The website will be used to promote Lifeline Racing.  It will provide information about the club, including the mission statement and a brief history of the organization.
+
+2. **Concept:**<br>
+The wireframing of the site was conceptualized and developed on mockingbird and diplayed below.  The site will developed on a single page and utilize jQuery and ajax to enhance user experience.
+
+    ![Wireframe](/index.png)
+
+3. **Process:**<br>
+The skeleton of the site will be laid out in HTML and CSS using Sass.
+
+<h2>Project Scope</h2>
+* Be hosted on Github Pages and be available to the web
+* Contain an organization structure for HTML, CSS, and Javascript
+* Utilize vanilla Javascript OR jQuery for DOM manipulation
+* Adequately represent your project scope; ie: a product website should contain product information.
+* Stick with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles
+* Use 2 media queries/breakpoints for Responsive Design
+* Use 1 $.ajax calls to external APIs and render data from them
--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 
   console.log("All resources are loaded");
-// contact submit event handler
 
+// hide status messages
+$('.loading').hide();
+$('.success').hide();
+$('.error').hide();
+
+// contact submit event handler
 $('#form_send').on('click', function() {
     var formdata = app.createFormObject();
+    console.log(formdata);
     console.log('Clicked form submit...');
+    app.sendEmail(formdata);
+
   });
 
 });
@@ -18,13 +26,51 @@ app.createFormObject = function() {
   retJson.userName = $('#user_name').val();
   retJson.userEmail = $('#user_email').val();
   retJson.request = $('#user_request').val();
-  retJson.type_road = $('#type_road').val();
-  retJson.type_cross = $('#type_cross').val();
-  retJson.type_track = $('#type_track').val();
-  retJson.other = $('#other').val();
 
-console.log(retJson);
+  // retJson.type_road = $('#type_road').val();
+  // retJson.type_cross = $('#type_cross').val();
+  // retJson.type_track = $('#type_track').val();
+  // retJson.other = $('#other').val();
+
+// testing...coment out when done
+// console.log(retJson);
 
   return retJson;
 
+}
+app.sendEmail = function(emailData) {
+
+  $('.loading').slideDown(); // .hide()
+  // hide old messages...because this is a new request
+  $('.success').hide();
+  $('.error').hide();
+
+/*
+  var ajaxData = {
+    url: 'http://imperialholonet.herokuapp.com/api/mail',
+    type: 'POST',
+    data: emailData,
+    success: function(data) {
+      console.log(data);
+      $('.loading').slideUp(); // .hide()
+      $('.success').show() // .slideDown()
+    },
+    error: function(err) {
+      console.log(err);
+      $('.loading').slideUp(); // .hide()
+      $('.error').show()
+    }
+};
+*/
+var ajaxData = {
+  url: 'http://imperialholonet.herokuapp.com/api/mail',
+  type: 'GET',
+  dataType: 'json',
+  success: function(data) {
+      console.log("success");
+      console.log(data);
+  },
+};
+
+$.ajax(ajaxData)
 }
